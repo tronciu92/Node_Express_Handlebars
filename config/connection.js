51732f7ac467dad3fb5a9f61f3bcb276e6a16234
Vasile <tronciu92@gmail.com>
2020-11-07 11:43:48 -0800

    db

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   connection = mysql.createConnection({
-      host: "localhost",
+      host: "lblonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       port: 3306,
-      user: "root",
-      password: "Rma4ever",
-      database: "burgers_db"
+      user: "e6ka3cy1aiesihwf",
+      password: "aup3mu238wu1i92w",
+      database: "vygdg78d3w23b2kx"
 });
 };
 
